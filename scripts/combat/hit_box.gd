a1a5b203933705c6b4d964bd3d5982389e653be9
Davi <fcodavibandeira@edu.unifor.br>
2022-06-19 18:03:34 -0300

    New Collision Logic

--- scripts/combat/hit_box.gd
+++ scripts/combat/hit_box.gd
@@ -1,32 +1,15 @@
 extends Area2D
 class_name HitBox
 
-onready var collision: CollisionShape2D = get_node("Collision")
-
 var health
 
 export(NodePath) onready var stats = get_node(stats) as Node
 export(NodePath) onready var parent = get_node(parent) as KinematicBody2D
 
 export(int, 10, 60, 5) var collision_radius = 10
-export(Vector2) var collision_offset
 
 func _ready() -> void:
 	health = stats.health
-	define_collision_radius()
-	
-	
-func define_collision_radius() -> void:
-	var physic_collision: CollisionShape2D = parent.get_node("Collision")
-	
-	var collision_shape: Shape2D = collision.get_shape()
-	var physic_collision_shape: Shape2D = physic_collision.get_shape()
-	
-	collision_shape.set_radius(collision_radius)
-	physic_collision_shape.set_radius(collision_radius)
-	
-	collision.position = collision_offset
-	physic_collision.position = collision_offset
 	
 	
 func update_health(area) -> void:
--- scripts/enemies/base/enemy.gd
+++ scripts/enemies/base/enemy.gd
@@ -3,8 +3,15 @@ class_name EnemyMain
 
 onready var sprite: Sprite = get_node("Texture")
 
+onready var hitbox: Area2D = get_node("HitBox")
 onready var in_range: Area2D = get_node("InRange")
 onready var detection_area: Area2D = get_node("DetectionArea")
+
+onready var areas_list: Array = [
+	hitbox,
+	in_range,
+	detection_area
+]
 
 var velocity: Vector2 = Vector2.ZERO
 
@@ -13,7 +20,24 @@ var death: bool = false
 var attack: bool = false
 
 export(int) var distance_threshold
+export(Vector2) var collision_offset
 
+func _ready() -> void:
+	configure_collision()
+	
+	
+func configure_collision() -> void:
+	var collision: CollisionShape2D = get_node("Collision")
+	var shape: Shape2D = collision.get_shape()
+	shape.set_radius(hitbox.collision_radius)
+	collision.position = collision_offset
+	
+	for area in areas_list:
+		var collision_shape: Shape2D = area.get_child(0).get_shape()
+		collision_shape.set_radius(area.collision_radius)
+		area.position = collision_offset
+		
+		
 func _physics_process(_delta: float) -> void:
 	move()
 	velocity = move_and_slide(velocity)
