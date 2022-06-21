d40446a1f961a6bdb462fa89970308595c20cfa7
Davi <fcodavibandeira@edu.unifor.br>
2022-06-21 13:46:53 -0300

    Portal (WIP) and Level Design

---
+++ scripts/interactables/teleport/portal.gd
@@ -0,0 +1,19 @@
+extends Area2D
+class_name TeleportPortal
+
+onready var animation: AnimationPlayer = get_node("Animation")
+onready var portal_texture: Sprite = get_node("PortalAnimation/MainSprite")
+
+var texture_dict: Dictionary = {
+	"start": [
+		
+	],
+	
+	"loop": [
+		
+	],
+	
+	"end": [
+		
+	]
+}
---
+++ scripts/management/debugger_level.gd
@@ -0,0 +1,6 @@
+extends Node2D
+class_name DebuggerLevel
+
+func _process(_delta: float) -> void:
+	#print(Engine.get_frames_per_second())
+	pass
